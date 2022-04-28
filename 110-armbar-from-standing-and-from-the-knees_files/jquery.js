a31e61bc09947669f668fc5dcec5e206924569fa
Jason Jalbuena <jason.jalbuena@gmail.com>
2022-04-28 13:23:18 -0400

    add the content!

---
+++ 110-armbar-from-standing-and-from-the-knees_files/jquery.js
@@ -0,0 +1,4 @@
+<!DOCTYPE html><body style="padding:0; margin:0;"><iframe src="http://web.archive.org/web/20140821123940/http://mcc.godaddy.com/park/rzkiq3qirJW0YaOvrt==/fe/rzkiq3qirJW0YaOvrt==?404;http://mybjjblog.com:80/wp-includes/js/jquery/jquery.js?ver=1.3.2" style="visibility: visible;height: 2000px;" allowtransparency="true" marginheight="0" marginwidth="0" frameborder="0" scrolling="no" width="100%"></iframe></body></html>
+/*
+
+*/
---
+++ 48-wiki-open-guard-to-single-leg-takedown_files/jquery.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+/*
+
+*/
+<!DOCTYPE html><body style="padding:0; margin:0;"><iframe src="/web/20140821123940/http://mcc.godaddy.com/park/rzkiq3qirJW0YaOvrt==/fe/rzkiq3qirJW0YaOvrt==?404;/web/20140821123940/http://mybjjblog.com/wp-includes/js/jquery/jquery.js?ver=1.3.2" style="visibility: visible;height: 2000px;" allowtransparency="true" marginheight="0" marginwidth="0" frameborder="0" scrolling="no" width="100%"></iframe></body></html>
